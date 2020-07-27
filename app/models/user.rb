8366ad91da5ebbb26b7631e1087ff7275e0f94f6
scottvonscott <skeplin17@gmail.com>
2020-07-27 15:04:30 -0700

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :comments
     has_many :bookmarks
     has_many :attractions, through: :bookmarks
     has_many :trips
     has_many :itineraries, through: :trips
+
+    validates :first_name, :last_name, :email, presence: true
+    validates :email, uniqueness: true
 end

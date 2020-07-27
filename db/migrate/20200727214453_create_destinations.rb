c859d2e375665517907dafa71c6b313e9a364fc9
scottvonscott <skeplin17@gmail.com>
2020-07-27 14:53:52 -0700

    add city to destination table

--- db/migrate/20200727214453_create_destinations.rb
+++ db/migrate/20200727214453_create_destinations.rb
@@ -1,6 +1,7 @@
 class CreateDestinations < ActiveRecord::Migration[6.0]
   def change
     create_table :destinations do |t|
+      t.string :city
       t.string :country
       t.string :continent
       t.string :primary_language

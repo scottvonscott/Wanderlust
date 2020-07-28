28a49350056967761dadc6b6662d307c559eb642
scottvonscott <skeplin17@gmail.com>
2020-07-28 11:29:01 -0700

    logged in login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     end
 
     def create
+        
         if @user = User.create(user_params)
             session[:user_id] = @user.id
             redirect_to user_path(@user)
@@ -22,7 +23,7 @@ class UsersController < ApplicationController
 private
 
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :email, :password)
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
    
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,14 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
 
+  #gonna need nested here
+  resources :trips
+  resources :itineraries
   resources :destinations
   resources :attractions
-  resources :itineraries
-  resources :trips
   resources :comments
+ 
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

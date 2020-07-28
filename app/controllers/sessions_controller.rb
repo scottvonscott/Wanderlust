e16d5a367d14ad4969d9ddb216421322889cc422
scottvonscott <skeplin17@gmail.com>
2020-07-27 17:03:03 -0700

    login/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,11 @@ class SessionsController < ApplicationController
     end
 
     def new
-        @user = User.new
     end
 
     def create
-        uuser = User.find_by_email(params[:email])
-        if user && u.authenticate(params[:password])
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             redirect_to user_path(user)
         else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,21 @@ class UsersController < ApplicationController
         if @user = User.create(user_params)
             session[:user_id] = @user.id
             redirect_to user_path(@user)
+            
         else
             redirect_to new_user_path
         end
     end
 
     def show
+        @user = User.find_by(id: params[:id])
+
     end
     
 private
 
     def user_params
-        params.require(:user).permit()
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
    
 

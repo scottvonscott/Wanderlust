c4079a202902572a4debeca227cd7aa1afa12e86
scottvonscott <skeplin17@gmail.com>
2020-07-27 18:18:48 -0700

    index markers, header links

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,23 @@
 class BookmarksController < ApplicationController
 
 
+    def index
+    end
 
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_bookmark
+    end
 end

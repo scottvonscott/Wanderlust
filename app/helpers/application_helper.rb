3fbe494dcf3e11b42c2317ad2642327f2d5637c4
scottvonscott <skeplin17@gmail.com>
2020-08-01 14:54:31 -0700

    started user_access method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,11 @@ module ApplicationHelper
     def logged_in?
         !!current_user
     end
+
+    def user_access
+        unless logged_in?
+            redirect_to root_path
+        end
+    end
+
 end

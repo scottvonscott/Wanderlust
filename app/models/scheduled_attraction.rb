4e130c8323e345f9acf5e3f839e159566bdf69d9
scottvonscott <skeplin17@gmail.com>
2020-08-01 13:10:20 -0700

    View refactoring

--- app/models/scheduled_attraction.rb
+++ app/models/scheduled_attraction.rb
@@ -2,7 +2,7 @@ class ScheduledAttraction < ApplicationRecord
   belongs_to :itinerary
   belongs_to :attraction
   validates :time_of_day, presence: true
-  validates :time_of_day, inclusion: {in: %w(morning afternoon evening), message: "%{value} is not a valid time to schedule an attraction"}
+  validates :time_of_day, inclusion: {in: %w(Morning Afternoon Evening), message: "%{value} is not a valid time to schedule an attraction"}
   accepts_nested_attributes_for :attraction, reject_if: proc {|attributes| attributes['name'].blank?}
 
 

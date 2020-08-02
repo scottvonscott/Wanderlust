8a4c197b0b5bfb4bb1995cbc26f6de0de27a7765
scottvonscott <skeplin17@gmail.com>
2020-08-02 11:56:57 -0700

    Errors for user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     
     def new
+        @user = User.new
     end
 
     def create
@@ -19,7 +20,7 @@ class UsersController < ApplicationController
 private
 
     def user_params
-        params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
    
 
